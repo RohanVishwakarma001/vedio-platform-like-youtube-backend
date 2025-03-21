858dcb0fa159169e766d7a3efa163b4563c651e5
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2025-03-21 17:33:27 +0530

    complete signup controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,6 +10,11 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "All fields are required" });
     }
 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
